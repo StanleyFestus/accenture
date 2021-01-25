686a2b608dea9dddd56efecf4fbf10f05bcacdf2
Stanley Festus <festusstanley@gmail.com>
2021-01-25 14:28:12 +0200

    Landing.js modified

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,61 +5,39 @@ import Dashboard from './Dashboard';
 import '../index.css';
 
 const initialState = {
-  loading: true,
-  error: false,
   places: [],
 }
 
 const Landing = () => {
 
   const [state, setState] = useState(initialState);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [currentPage, setCurrPage] = useState(0);
 
   const getData = async() => {
     const res = await axios.get(`http://localhost:5000/api/places`)
     .catch(function(err){
-      console.log(err)
-      setState({
-        ...state,
-        error: true
-      })
+      setError(true);
     })
     
-
     const data = res.data;
     console.log(res)
 
     if(data){
-      setState({
-        ...state, 
-        places: data, 
-        loading: false, 
-        error: false
-      })
-    } else if(!data){
-      setState({
-        ...state,
-        loading: true,
-        error: false,
-      })
-    } else {
-      setState({
-        loading: false,
-      })
+      setState({...state, places: data})
+      setLoading(false)
     }
   }
 
   useEffect(() => {
     getData()
-    return () => {
-      console.log('cleaned')
-    }
   }, [])
 
   // Helper function to render if shop is open or close
   const openingHour = (open, close) => {
-    var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes();
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes();
     
     if(open === null && close === null){
       return 'No info about opening hours'
@@ -79,31 +57,34 @@ const Landing = () => {
 
   //Conditonally rendered component
   const currentPageData = () => {
-    if(state.loading){
-      return <h1 
-      className="text-center">
-        Loading...</h1>
+
+    if(loading){
+      return <h1 className="text-center">Loading...</h1>
+
     } else if(state.places){
       return state.places.slice(offset, offset + PER_PAGE)
         .map((place) =>{
         let time = place.opening_hours.hours;
-        for(var key in time){
+        const {id, name, location:{address:{street_address, postal_code, locality}}} = place;
+        for(let key in time){
           // console.log(time[key].opens)
           open =  time[key].opens;
           closed = time[key].closes;
         }
+
         return (
             <Dashboard 
-             key={place.id} 
-            name={place.name.fi}
-            street={place.location.address.street_address}
-            postCode={place.location.address.postal_code}
-            locality={place.location.address.locality}
+            key={id} 
+            name={name.fi}
+            street={street_address}
+            postCode={postal_code}
+            locality={locality}
             open={openingHour(open, closed)}/>
-        )
-      })
-    }  else {
-      return <div>Something went wrong!</div>
+            )
+          })
+        
+      }  else if(error){
+      return <div>An error occurred!</div>
     }
   }
   
@@ -125,7 +106,7 @@ const Landing = () => {
      </div>
       <div>
         {state.places && <ReactPaginate
-        style={{width: '300px'}}
+          style={{width: '300px'}}
           previousLabel={"← Previous"}
           nextLabel={"Next →"}
           pageCount={pageCount}
