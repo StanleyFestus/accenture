6c56f3195d13ba28050ff072fecd3de5c20d0879
Stanley Festus <festusstanley@gmail.com>
2021-01-25 18:20:14 +0200

    Backend index.js modified

--- index.js
+++ index.js
@@ -64,13 +64,6 @@ app.get('/api/*', (req, res, next) => {
   })
 })
 
-if (['production'].includes(process.env.NODE_ENV)) {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
